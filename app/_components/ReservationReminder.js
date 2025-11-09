efd679eb039512fda22514f5affe791ebc4e9e23
sanjeevan <sanjeevan.g0@gmail.com>
2025-11-09 21:50:30 +0530

    added reservation module

--- app/_components/ReservationReminder.js
+++ app/_components/ReservationReminder.js
@@ -1,9 +1,12 @@
+"use client";
+
 import { XMarkIcon } from "@heroicons/react/24/solid";
 import { format } from "date-fns";
+import { useReservation } from "./ReservationContext";
 
 function ReservationReminder() {
   // CHANGE
-  const range = { from: null, to: null };
+  const { range, resetRange } = useReservation();
 
   if (!range.from || !range.to) return null;
 
@@ -14,7 +17,10 @@ function ReservationReminder() {
         {format(new Date(range.from), "MMM dd yyyy")} to{" "}
         {format(new Date(range.to), "MMM dd yyyy")}
       </p>
-      <button className="rounded-full p-1 hover:bg-accent-600 transition-all">
+      <button
+        className="rounded-full p-1 hover:bg-accent-600 transition-all"
+        onClick={resetRange}
+      >
         <XMarkIcon className="h-5 w-5" />
       </button>
     </div>
